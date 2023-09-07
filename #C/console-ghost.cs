662989248ff14fb35b8b850cd83d15b0781b40a3
thebellywolf <cdspenzer@gmail.com>
2023-09-07 13:18:23 -0400

    added C# file to projects and willy the console ghost

---
+++ #C/console-ghost.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // These commands print the individual Strings in their parenthesis to the console to create the image of a Ghost named Willy.
+      Console.WriteLine("      .-."); 
+      Console.WriteLine("     (o o)");
+      Console.WriteLine("     | O |");
+      Console.WriteLine("     |   |");
+      Console.WriteLine("     '~~~'");
+      Console.WriteLine("Willy the Ghost");
+     }
+  }
+}
